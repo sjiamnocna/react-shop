e4b432e9904bc087291cf8d7d32e7b3eb9a3479a
Your Name <you@example.com>
2020-04-26 20:03:45 +0200

    Items and header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.comp';
 import HomePage from './pages/homepage/homepage.comp';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.comp';
 
 function App() {
   return (
     <div>
+      <Header/>
+      <switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </switch>
     </div>
   );
 }
---
+++ src/components/menu/item/menu-item.comp.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './menu-item.scss';
+
+
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} className='background-image'/>
+        <div className='content'>
+            <h2 className='title'>{title.toUpperCase()}</h2>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default withRouter(MenuItem);
