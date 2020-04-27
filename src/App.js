708aa49319899855f69fd4dd9c858a3223ed67c8
Your Name <you@example.com>
2020-04-27 11:23:58 +0200

    Repaired Switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Header from './components/header/header.comp';
 import HomePage from './pages/homepage/homepage.comp';
 import ShopPage from './pages/shop/shop.comp';
+import SignInAndSignUpPage from './pages/login/login.comp';
 
 function App() {
   return (
     <div>
       <Header/>
-      <switch>
+      <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
-      </switch>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>
     </div>
   );
 }
