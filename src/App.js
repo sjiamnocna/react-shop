1a8aae2350e7893697a3e177808d408c3d7fb665
sjiamnocna <jancikuvmail@gmail.com>
2020-06-06 20:39:19 +0200

    Sing up with e-mail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.comp';
-import HomePage from './pages/homepage/homepage.comp';
-import ShopPage from './pages/shop/shop.comp';
-import SignInAndSignUpPage from './pages/login/login.comp';
+import HomePage from './pages/homepage/homepage.page';
+import ShopPage from './pages/shop/shop.page';
+import SignInAndSignUpPage from './pages/login/login.page';
 
 import './App.css';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,8 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state.currentUser);
+        });
+      }
+
+      this.setState({currentUser: userAuth});
     });
   }
 
--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './login.scss';
 
 import SignIn from '../../components/sing-in/signin.comp';
+import SignUp from '../../components/sing-up/signup.comp';
 
 const SingInAndSignUpPage = ()=>(
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
     
 );
