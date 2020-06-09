3e6b72ec780fbf9c915d7e146e7a6f0f080d0aab
sjiamnocna <jancikuvmail@gmail.com>
2020-06-09 13:19:04 +0200

    Connected HEADER

--- src/components/header/header.comp.jsx
+++ src/components/header/header.comp.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 
+import { connect } from 'react-redux';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.scss';
@@ -29,4 +31,12 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+/**
+ * 
+ * @param {*} state 
+ */
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
