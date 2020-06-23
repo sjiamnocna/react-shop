85676563702e03bef647218343c3d4a0ce735b9f
sjiamnocna <jancikuvmail@gmail.com>
2020-06-23 17:03:59 +0200

    Repaired styles

--- src/components/cart/dropdown/dropdown.comp.jsx
+++ src/components/cart/dropdown/dropdown.comp.jsx
@@ -8,12 +8,13 @@ import './dropdown.scss';
 
 const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
         {
             cartItems.map(cartItem => (
                 <CartItem key={cartItem.id} item={cartItem} />
             ))
         }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
