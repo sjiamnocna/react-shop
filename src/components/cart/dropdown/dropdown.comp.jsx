c57f667f3e9f07507e945f2fb72128e4b0cd42ae
sjiamnocna <jancikuvmail@gmail.com>
2020-06-23 13:18:23 +0200

    Dropdown groups

--- src/components/cart/dropdown/dropdown.comp.jsx
+++ src/components/cart/dropdown/dropdown.comp.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../../button/button.comp';
+import CartItem from '../item/item.comp';
 
 import './dropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
         <div className='cart-items' />
+        {
+            cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))
+        }
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({cart: { cartItems}}) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
