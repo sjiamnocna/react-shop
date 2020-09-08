fabc56879df29f482aa482a8a06f569678df785e
sjiamnocna <jancikuvmail@gmail.com>
2020-09-08 18:45:25 +0200

    Moved shop data to object

--- src/components/collection/overview/collection-ov.comp.jsx
+++ src/components/collection/overview/collection-ov.comp.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../../redux/shop/shop.selector';
 
 import CollectionPreview from '../preview/collection-preview';
-
-
 
 import './collection-ov.scss';
 
@@ -21,7 +19,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
