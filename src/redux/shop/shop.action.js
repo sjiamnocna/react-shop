58bd112b62ea52ac10084bd85ab90935b348964d
jancsi <janca@logio.cz>
2020-11-17 10:39:22 +0100

    Video 183. Redux-Thunk finally working

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,6 +1,31 @@
 import ShopActionTypes from './shop.types';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+const fetchCollectionsStart = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+const fetchCollectionsFail = errm => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAIL,
+  payload: errm
+});
+
+const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart())
+
+    collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      
+      dispatch(fetchCollectionsSuccess(collectionsMap))
+    }).catch(errm => dispatch(fetchCollectionsFail(errm.message)));
+  }
+}
+
+export default fetchCollectionsStartAsync;
